f06e641267d1824a88ce118951b994e7ed16dd70
hadi-tghl <hadi@gmx.se>
2021-12-04 19:16:25 +0100

    Initialazing the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# TollCalculator
+TollCalculator initializing
---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TollCalcuilator tollCalculator = new TollCalcuilator();
+
+    }
+}
